0825c4e144839c17c33342f06f8f30afa999c0f3
Berk Ott <berkott3002@gmail.com>
2023-12-05 15:20:42 -0500

    added better reduced rank model

---
+++ src/models/reduced_rank_model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+
+
+class ReducedRankModel(nn.Module):
+    """
+    Better reduced rank model.
+    """
+    def __init__(
+        self, 
+        n_recordings: int,
+        n_neurons_per_recording: list, 
+        n_time_bins: int, 
+        rank: int
+    ):
+        self.n_recordings = n_recordings
+        self.n_neurons_per_recording = n_neurons_per_recording
+        self.rank = rank
+        self.n_time_bins = n_time_bins
+        
+        self.Us = [nn.Parameter(torch.randn(self.n_neurons_per_recording[i], self.rank)) for i in range(self.n_recordings)]
+        self.V = nn.Parameter(torch.randn(self.rank, self.n_time_bins))
+        # self.biases = [nn.Parameter(torch.randn(self.n_neurons_per_recording[i], self.n_time_bins)) for i in range(self.n_recordings)]
+        self.bias = nn.Parameter(torch.randn(self.n_time_bins))
+
+    def forward(self, recording_index, recording_X):
+        # TODO: Check which dim to do average over
+        Y_hat = torch.average((self.Us[recording_index] @ self.V).T @ recording_X, dim=1) + self.bias
+        return Y_hat
